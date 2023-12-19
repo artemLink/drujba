b8929071aae37e5a42fd7928dd836da301a8d511
DarkSttar <llldarklllstarlll@gmail.com>
2023-12-19 22:21:23 +0200

    Create FolderPath.py

---
+++ FolderPath.py
@@ -0,0 +1,5 @@
+import os
+current_directory = os.getcwd()
+FOLDER_ACCOUNTS_PATH = os.path.join(current_directory,'Accounts')
+FOLDER_ADDRESSBOOKS_PATH = os.path.join(current_directory,'AddresBooksFolder')
+FOLDER_NOTESBOOKS_PATH = os.path.join(current_directory,'NotesBooksFolder')
