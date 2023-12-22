f6cf48d6c3cf7f690a5c3e097ba5880cfc20dbd3
DarkSttar <llldarklllstarlll@gmail.com>
2023-12-22 17:37:54 +0200

    Update FolderPATH

--- FolderPath.py
+++ FolderPath.py
@@ -7,6 +7,8 @@ FOLDER_NOTESBOOKS_PATH = os.path.join(current_directory,'NotesBooksFolder')
 
 
 
+CONTACTS = os.path.join(current_directory,'Contacts.json')
+NOTES = os.path.join(current_directory,'Notes.json')
 
 def create_folders():
     if not os.path.exists(FOLDER_ACCOUNTS_PATH):
@@ -15,6 +17,7 @@ def create_folders():
         os.makedirs(FOLDER_ADDRESSBOOKS_PATH)
     if not os.path.exists(FOLDER_NOTESBOOKS_PATH):
         os.makedirs(FOLDER_NOTESBOOKS_PATH)
+<<<<<<< Updated upstream
 CONTACTS = os.path.join(current_directory,'Contacts.json')
 NOTES = os.path.join(current_directory,'Notes.json')
 
@@ -27,3 +30,11 @@ def create_base_json_files():
     #     os.makedirs(CONTACTS)
     # if not os.makedirs(NOTES):
     #     os.makedirs(NOTES)
+=======
+
+def create_base_json_files():
+    if not os.path.exists(CONTACTS):
+        os.makedirs(CONTACTS)
+    if not os.makedirs(NOTES):
+        os.makedirs(NOTES)
+>>>>>>> Stashed changes
